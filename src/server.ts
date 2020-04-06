d610e2cd68370fb340148d37a4117f710b74fbd3
Davis Kauffmann <daviskauffmann@gmail.com>
2020-04-06 02:21:17 -0400

    Use app.listen

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
-import http from 'http';
 import mongoose from 'mongoose';
 import app from './app';
-
-const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URI!, {
     useNewUrlParser: true,
@@ -12,7 +9,7 @@ mongoose.connect(process.env.MONGODB_URI!, {
     if (err) throw err;
     // TODO: seed with admin user
     console.log(`Connected to ${process.env.MONGODB_URI}`);
-    server.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, () => {
         console.log(`Server listening on port ${process.env.PORT}`);
     });
 });
