228a2c80ae6ca4911f25200f240c16d6e7722bb7
Davis Kauffmann <daviskauffmann@gmail.com>
2021-08-01 12:18:57 -0400

    Insert default admin user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import dotenv from 'dotenv';
-import sourceMapSupport from 'source-map-support';
-
-sourceMapSupport.install();
-dotenv.config();
-
 console.log(`Application is in ${process.env.NODE_ENV} mode`);
 
 import './server';
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import app from './app';
 import sequelize from './sequelize';
+import bcrypt from 'bcrypt';
 
 sequelize.authenticate()
     .then(() => console.log(`Connected to database: ${process.env.DATABASE_URI}`))
     .then(() => sequelize.sync())
-    // .then(() => sequelize.query(`
-    //     CREATE IF NOT EXISTS "Users"
-    // `))
+    .then(async () => sequelize.query(`
+        INSERT INTO "user" ("id", "username", "password", "email", "verified", "roles")
+        SELECT
+            1 AS "id",
+            'admin' AS "username",
+            '${await bcrypt.hash('admin', 12)}' AS "password",
+            'admin@admin.com' AS "email",
+            true as "verified",
+            ARRAY['admin'] AS "roles"
+        WHERE NOT EXISTS (
+            SELECT "id"
+            FROM "user"
+            WHERE "id" = 1
+        )
+    `))
     .then(() => {
         const port = process.env.PORT || 3000;
         app.listen(port, () => {
